d3209617c8fe352a005bc5895ca63757cea67eac
Chai <isaigomezhernandez@gmail.com>
2020-01-14 13:00:08 -0600

    Esilos en el componente weatherTemperature

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -22,17 +22,21 @@ const icons = {
 
 const getWeatherIcon = weatherState => {
   const icon = icons[weatherState];
+  const sizeIcon = "4x";
   if(icon)
-    return <WeatherIcons name ={icon} size="2x"/>;  
+    return <WeatherIcons className="wicon" name ={icon} size={sizeIcon}/>;  
   else
-    return <WeatherIcons name ={ "day-sunny" } size="2x"/>;  
+    return <WeatherIcons className="wicon" name ={ "day-sunny" } size={sizeIcon}/>;  
 }
 
 const WeatherTemperature = ({temperature, weatherState}) => {
   return (
   <div className="weatherTemperatureCont">
-   { getWeatherIcon(weatherState) }
-    <span>{`${temperature} C°`}</span>
+   { 
+   getWeatherIcon(weatherState) 
+   }
+    <span className="temperature">{`${temperature}`}</span>
+    <span className="temperatureType">{`C°`}</span>
   </div>
   );
 };
