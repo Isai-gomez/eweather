3237488d2331332787a1c2eb19db12156b4a9ad1
Chai <isaigomezhernandez@gmail.com>
2020-01-15 11:24:11 -0600

    manejo de State con onclick

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,6 +4,7 @@ import WeatherData from "./WeatherData/";
 import './styles.css';
 import {
   SUN,
+  WINDY,
 } from './../../constants/weathers';
 /*Objeto con los datos de la temperatura que se
   envian al componente WeatherData.
@@ -14,13 +15,34 @@ const data = {
   humidity: 10,
   wind: '10 m/s',
 }
+const data2 = {
+  temperature: 34,
+  weatherState: WINDY,
+  humidity: 30,
+  wind: '15 m/s',
+}
 
 class WeatherLocation extends Component {
+  constructor(){
+    super();
+    this.state = {
+      city:"Tabasco",
+      data:data
+    }
+  }
+  handlerUpdateClick = () => {
+    this.setState({
+      city:"Comalcalco",
+      data:data2,
+    })
+  }
   render(){
+    const { city, data } = this.state;
   return (
     <div className="weatherLocationCont">
-      <Location city={"Tabasco"} />
+      <Location city={city} />
       <WeatherData data={data}/>
+      <button onClick={this.handlerUpdateClick}>Update</button>
     </div>
   );
   }
