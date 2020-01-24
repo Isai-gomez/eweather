ede492cd9f14e74944cbb685aa172e59ba7b59f2
Chai <isaigomezhernandez@gmail.com>
2020-01-23 22:23:45 -0600

    component locationList implementado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import WeatherLocation from "./components/WeatherLocation/";
+import LocationList from "./components/LocationList";
 
 import "./App.css";
 
@@ -7,7 +7,7 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      <WeatherLocation city={"Comalcalco,MX"}/>
+      <LocationList />
     </div>
   );
 }
