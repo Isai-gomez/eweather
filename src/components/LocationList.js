9ad6f2c8632f67b4ad275c856edfc4c55693d063
Chai <isaigomezhernandez@gmail.com>
2020-01-30 14:50:30 -0600

    iniciliando en null el state de app

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -3,8 +3,7 @@ import WeatherLocation from './WeatherLocation/'
 import PropTypes from 'prop-types';
 import './styles.css'
 const LocationList = ({ cities, onSelectedLocation })  => {
-    const handleWeatherLocationClick = city => {
-        console.log("handleWeatherLocationClick");
+    const handleWeatherLocationClick = city => {       
         onSelectedLocation(city);
     }
     const strComponents = cities =>{
