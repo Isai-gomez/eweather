9bde25c786ad1e44fc774c270c5d5b6b4dd46590
Isai-gomez <isaigomezhernandez@gmail.com>
2020-02-09 21:27:58 -0600

    mapeando para tener datos mas filtrados

---
+++ src/services/transformForecast.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+import transformWeather from './transforWeather';
+import 'moment/locale/es';
+const transformForecast = data => (
+    data.list.filter( item => (
+        moment.unix(item.dt).hour() === 6 ||
+        moment.unix(item.dt).hour() === 12 ||
+        moment.unix(item.dt).hour() === 18
+    )).map(item => (
+        {
+            weekday:moment.unix(item.dt).format('ddd'),
+            hour: moment.unix(item.dt).hour(),
+            data: transformWeather(item)
+        }
+    ))
+);
+export default transformForecast;
